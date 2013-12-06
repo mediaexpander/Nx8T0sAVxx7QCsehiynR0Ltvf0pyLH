6f90acd102451b57aa5f5f6931197ebc3a574040
mediaexpander <VICTORGS3@MacBook-Air-de-Victor.local>
2013-12-06 19:54:27 +0100

    c12

--- src/main/java/com/stackmob/customcode/util/HttpRequest.java
+++ src/main/java/com/stackmob/customcode/util/HttpRequest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.stackmob.example.util;
+package com.stackmob.customcode.util;
 
 import com.stackmob.core.ServiceNotActivatedException;
 import com.stackmob.core.customcode.CustomCodeMethod;
--- src/main/java/com/stackmob/customcode/util/ReadParams.java
+++ src/main/java/com/stackmob/customcode/util/ReadParams.java
@@ -19,7 +19,7 @@ package com.stackmob.example.util;
 import com.stackmob.core.customcode.CustomCodeMethod;
 import com.stackmob.core.rest.ProcessedAPIRequest;
 import com.stackmob.core.rest.ResponseToProcess;
-import com.stackmob.example.Util;
+import com.stackmob.customcode.Util;
 import com.stackmob.sdkapi.LoggerService;
 import com.stackmob.sdkapi.SDKServiceProvider;
 import com.stackmob.sdkapi.SMInt;
